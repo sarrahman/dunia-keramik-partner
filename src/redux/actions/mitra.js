0adc5e627d1a1e78fe894d5f5747deb7196f09e4
muhammad nur rahman <99724372+sarrahman@users.noreply.github.com>
2022-05-08 19:23:58 +0800

    Update mitra.js

--- src/redux/actions/mitra.js
+++ src/redux/actions/mitra.js
@@ -1,9 +1,15 @@
 import api from "../../utils/api";
 
 export const createMitra = (data) => (dispatch) => {
+  const {namaLengkap, email, noHp, alamat} = data;
   return new Promise((resolve, reject) => {
     api
-      .post(`/mitra`, data)
+      .post(`/mitra`, {
+      name: namaLengkap,
+      email,
+      phone: noHp,
+      address: alamat
+    })
       .then((response) => {
         resolve(response);
       })
