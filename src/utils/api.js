796b0aa9bada2a1a428cdacfd56ea4275d8d6462
muhammad nur rahman <99724372+sarrahman@users.noreply.github.com>
2022-05-08 10:24:45 +0800

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 
-const baseURL =
-  process.env.NODE_ENV === "production"
-    ? "https://dunia-keramik.herokuapp.com/api"
-    : "http://localhost:5000/api";
+const baseURL = "https://dunia-keramik.herokuapp.com/api";
+
+// const baseURL =
+//   process.env.NODE_ENV === "production"
+//     ? "https://dunia-keramik.herokuapp.com/api"
+//     : "http://localhost:5000/api";
 
 export default axios.create({
-  baseURL,
+  baseURL
 });
